502f1c518ebfa198ecfe066043229e09b4240970
J.C. Hiatt <jc@jchiatt.com>
2019-09-29 15:10:37 -0500

    feat(components): screensaver automatically triggers after 5 seconds of inactivity

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import StandardLayout from '../layouts/StandardLayout';
 import { Button, Icon, Modal, ProgressBar } from '@react95/core';
 import VisitorCounter from '../components/VisitorCounter';
+import ScreenSaver from '../components/ScreenSaver';
 
 const startDate = new Date(2019,9,19); // October 19, 2019
 const endDate = new Date(2025,9,14); // October 14, 2025
@@ -10,17 +11,23 @@ const progressSinceStart = Math.abs(today - startDate);
 const totalTimeDuration = Math.abs(endDate - startDate);
 const progress = Math.round((progressSinceStart/totalTimeDuration) * 100);
 
-export default () => (
-  <StandardLayout>
-    <marquee>🔥🔥🔥</marquee>
-    <VisitorCounter />
-    <Button>Click me!</Button>
-    <ProgressBar percent={progress} />
-    <Icon height={48} name="computer" width={48} />
-    <Icon height={48} name="recycle_file" width={48} />
-    <Icon height={48} name="recycle_empty" width={48} />
-    <Icon height={48} name="mute" width={48} />
-    <Icon height={48} name="unmute" width={48} />
-    <Modal defaultPosition={{ x: 200, y: 200 }}>test</Modal>
-  </StandardLayout>
-)
+export default () => {
+
+  return (
+    <>
+        <ScreenSaver />
+        <StandardLayout>
+          <VisitorCounter />
+          <Button>Click me!</Button>
+          <ProgressBar percent={progress} />
+          <Icon height={48} name="computer" width={48} />
+          <Icon height={48} name="recycle_file" width={48} />
+          <Icon height={48} name="recycle_empty" width={48} />
+          <Icon height={48} name="mute" width={48} />
+          <Icon height={48} name="unmute" width={48} />
+          <Modal defaultPosition={{ x: 200, y: 200 }}>test</Modal>
+        </StandardLayout>
+      )}
+    </>
+  )
+}
