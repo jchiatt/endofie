83e5ac618c4565889cec496a1e1cc61895f4ee8f
J.C. Hiatt <jc@jchiatt.com>
2019-09-29 16:36:51 -0500

    chore: remove unused imports on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import StandardLayout from '../layouts/StandardLayout';
-import { Button, Icon, Modal } from '@react95/core';
 import SpinningLogo from '../components/SpinningLogo';
 import Flames from '../components/Flames';
 import ScreenSaver from '../components/ScreenSaver';
