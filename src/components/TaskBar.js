a663bb2604d9e563a3020f8accefd6dcc0b79114
Kayla Sween <kayla@kaylasween.com>
2019-09-29 17:20:46 -0500

    adding countdown

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Button, Icon } from '@react95/core';
+import countdown from '../util/countdown';
 
 import '../styles/taskBar.css';
 
 export default function TaskBar() {
+    const [years, days, hours, minutes, seconds] = countdown();
     return (
         <div className="taskbar">
             <Button className="taskbar-btn">
                 <Icon height={16} name="logo" width={16} /> Start
             </Button>
             <div className="countdown">
-                900 days 21:22:22
+                {years} years, {days} days {hours}:{minutes}:{seconds}
             </div>
         </div>
     )
