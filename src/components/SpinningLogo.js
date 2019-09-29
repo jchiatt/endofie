a3b64ab9fc7d130224d032f401d89ea1507936ff
J.C. Hiatt <jc@jchiatt.com>
2019-09-29 16:37:52 -0500

    fix: add empty alt text to SpinningLogo

--- src/components/SpinningLogo.js
+++ src/components/SpinningLogo.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../styles/spin.css';
 
 export default function SpinningLogo() {
-    return <img src={'./logo.png'} className="spin-img" />;
+    return <img src={'./logo.png'} className="spin-img" alt="" />;
 }
