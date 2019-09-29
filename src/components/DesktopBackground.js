b42cf376e0456f7cdd7cc4cc8326b37c8d044387
J.C. Hiatt <jc@jchiatt.com>
2019-09-29 16:31:02 -0500

    styles: change desktop background color every second

--- src/components/DesktopBackground.js
+++ src/components/DesktopBackground.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
+import VisitorCounter from './VisitorCounter';
 
 function getRandomColor() {
   return '#' + Math.random().toString(16).substring(2, 8);
@@ -9,7 +10,7 @@ const Background = styled.div`
   position: relative;
   height: 100vh;
   width: 100vw;
-  background-color: ${getRandomColor()};
+  background-color: ${props => props.color ? props.color : getRandomColor()};
   background-image: url('/logo.png');
   background-repeat: no-repeat;
   background-position: center;
@@ -19,14 +20,39 @@ const Background = styled.div`
     top: calc(50% + 300px);
     left: 50%;
     transform: translate(-50%, -50%);
+    width: 75%;
+    padding: 2rem;
     font-size: 4rem;
     font-weight: bold;
     color: #fff;
     text-align: center;
     background-color: #111;
   }
-`
+`;
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+  text-align: center;
+`;
 
 export default function DesktopBackground({ children }) {
-  return <Background>{children}</Background>
+  const [color, setColor] = React.useState(getRandomColor());
+  
+  React.useEffect(() => {
+    let interval = setInterval(() => {
+      setColor(getRandomColor());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [color]);
+
+
+  return <Background color={color}>
+    {children}
+    <Footer>
+      <VisitorCounter />
+    </Footer>
+    </Background>
 }
