3173c2534497acc98134e786b69f6fe94b7c6a01
Kayla Sween <kayla@kaylasween.com>
2019-09-29 21:14:20 -0500

    formatting the details modal.

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Modal } from '@react95/core';
 
+import '../styles/detailsModal.css';
+
 export default function DetailsModal() {
     return (
         <Modal title="WHERE THE PARTY AT">
             <h3>The deets</h3>
-            <div>Where: We don't know yet.</div>
-            <div>When: October 14, 2025</div>
-            <div>Why: why tf not?</div>
+            <ul>
+                <li><strong>Where:</strong> We don't know yet.</li>
+                <li><strong>When:</strong> October 14, 2025</li>
+                <li><strong>Why:</strong> why tf not?</li>
+            </ul>
         </Modal>
     )
 }
