d1911f1cd9690b83336590f046940e99e5ed1c24
J.C. Hiatt <jc@jchiatt.com>
2019-10-03 11:01:52 -0500

    feat(components): hook up Navigation to TaskBar component, move Modal styles into styled-components

--- src/components/DetailsModal.js
+++ src/components/DetailsModal.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import { Modal } from '@react95/core';
+import styled from 'styled-components';
 
-import '../styles/detailsModal.css';
+const StyledModal = styled(Modal)`
+  top: 6rem;
+  right: 2rem;
+  width: 300px;
+  min-height: 300px;
+
+  @media screen and (min-width: 480px) {
+    width: 400px;
+  }
+
+  h3 {
+    margin-bottom: 0;
+  }
+`
 
 export default function DetailsModal() {
     return (
-        <Modal title="WHERE THE PARTY AT">
-            <h3>The deets</h3>
-            <ul>
-                <li><strong>Where:</strong> We don't know yet.</li>
-                <li><strong>When:</strong> October 14, 2025</li>
-                <li><strong>Why:</strong> why tf not?</li>
-            </ul>
-        </Modal>
+      <StyledModal title="WHERE THE PARTY AT">
+        <h3>The deets</h3>
+        <p>
+          On October 14, 2025, Microsoft will officially stop supporting IE 11.
+        </p>
+        <p>Get ready to party.</p>
+        <ul>
+          <li>
+            <strong>Where:</strong> We don't know yet.
+          </li>
+          <li>
+            <strong>When:</strong> October 14, 2025
+          </li>
+          <li>
+            <strong>Why:</strong> why tf not?
+          </li>
+          <li>
+            <strong>Cost:</strong> FREE (
+            <a href="mailto:jc+endofie@jchiatt.com">if we have sponsors</a>)
+          </li>
+        </ul>
+      </StyledModal>
     )
 }
