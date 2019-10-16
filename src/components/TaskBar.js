6162a8d9fad96a37e503fb6a41868884723baa96
Kayla Sween <kayla@kaylasween.com>
2019-10-15 22:44:15 -0500

    changing start icon to logo.

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -13,7 +13,7 @@ export default function TaskBar() {
         {navIsActive && <Navigation toggleNav={toggleNav} />}
         <div className="taskbar">
           <Button className="taskbar-btn" onClick={toggleNav}  aria-expanded={navIsActive ? true : false}>
-            <Icon height={16} name="logo" width={16} /> 
+            <img src={'./logo.png'} height={16} name="logo" width={16} alt="" /> 
             <span>{navIsActive ? 'Close' : 'Start'}</span>
           </Button>
           <div className="countdown">
